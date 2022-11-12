7c1451860c1ac949d967e9fd835485552f632afc
bluesauce838 <94112413+bluesauce838@users.noreply.github.com>
2022-11-12 12:16:39 +0530

    2 files modified

--- Foo.js
+++ Foo.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 class Foo extends Component {
   handleClick = () => {
     console.log('Click happened');
+
+    fetch('https://dummyjson.com/products')
+      .then((res) => res.json())
+      .then(console.log);
   };
+
   render() {
     return <button onClick={this.handleClick}>Click Me</button>;
   }
